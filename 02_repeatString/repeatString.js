8edbd2e90e902ddcc0c501700a077e73bc96c1ea
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-03-21 09:53:57 -0700

    Complete Exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, timesRepeated ) {
+    let stringRepeated = '';
+    if (timesRepeated < 0) {
+        return 'ERROR';
+    } else {
+        for (let i = 0; i < timesRepeated; i++){
+            stringRepeated += string;
+        }
+        return stringRepeated;
+    }
 };
 
 // Do not edit below this line

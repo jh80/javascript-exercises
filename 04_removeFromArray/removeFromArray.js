d5b0adda81df6a9ffe1ab13bf75691ed0b874bb2
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-03-21 16:15:30 -0700

    Write removeFromArray function to where it can pass the first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,21 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, toRemove) {
+  const newArray = array;
+    for (let i = 0; i < newArray.length; i++) {
+       if (newArray[i] === toRemove) {
+        newArray.splice (i, 1);
+       }
+    }
+    return newArray;
 };
+const array1 = [1, 2, 3, 4, 5];
+console.log(array1);
+removeFromArray(array1, 2);
+
 
 // PSUEDOCODE
 // iterate over array
-  // when a match comes 
+  // when a match comes remove match from array -- using splice() method
+
 
 // Do not edit below this line
 module.exports = removeFromArray;

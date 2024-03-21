a7f2a043586560ff35e58de881adc86b7d000dc7
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-03-21 12:55:51 -0700

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringAsArray = string.split("");
+    stringAsArray.reverse();
+    const reversedString = stringAsArray.join("");
+    return reversedString;
 };
+
+// PSUEDOCODE
+// Turn string into an array using -- split("")
+// reverse array using -- reverse()
+// turn array into a string -- using join("")
 
 // Do not edit below this line
 module.exports = reverseString;

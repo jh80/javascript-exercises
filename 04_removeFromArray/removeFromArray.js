8323f233435b26ebcd0ff9e3acfdaf1a5be9df2b
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-03-22 08:23:55 -0700

    Complete 04 to where it passes all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,20 +1,22 @@
-const removeFromArray = function(array, toRemove) {
+const removeFromArray = function(array, ...args) {
   const newArray = array;
+  for (let j = 0; j < args.length; j++) {
     for (let i = 0; i < newArray.length; i++) {
-       if (newArray[i] === toRemove) {
+       if (newArray[i] === args[j]) {
         newArray.splice (i, 1);
+        --i;
        }
     }
-    return newArray;
+  }
+  return newArray;
 };
-const array1 = [1, 2, 3, 4, 5];
-console.log(array1);
-removeFromArray(array1, 2);
+
 
 
 // PSUEDOCODE
-// iterate over array
-  // when a match comes remove match from array -- using splice() method
+// iterate through args
+  // iterate over array
+    // when a match comes remove match from array -- using splice() method
 
 
 // Do not edit below this line

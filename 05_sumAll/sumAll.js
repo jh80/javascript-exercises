994c839c3b27867f76e26bb90c862fa000cbe586
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-03-22 11:03:11 -0700

    Revise exercise 05 to be more concise
    After looking at the solution I was able to see ways
    I could have been more consice in my code. In order to
    practice those ways I rewrote my code to apply them.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,28 @@
 const sumAll = function(num1, num2) {
+    let sum = 0;
+
+    if (num1 < 0 || num2 < 0) return 'ERROR';
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return 'ERROR';
+    if (num2 < num1) {
+        const temp = num1;
+        num1 = num2;
+        num2 = temp;
+    } 
+
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+    }
+    return sum;
+};
+
+/*
+// This is my original answer, above shows changes I made after looking at the solution
+
+const sumAll = function(num1, num2) {
     let smallNum;
     let bigNum;
     let sum = 0;
-    // CHANGE TO SWITCH SINTAX FOR PRACTICE
+
     if (num1 < 0 || 
         num2 < 0 || 
         (typeof num1 != 'number') || 
@@ -24,8 +44,7 @@ const sumAll = function(num1, num2) {
     }
     return sum;
 };
-
-
+*/
 
 // PSUEDOCODE
 // Compare numbers to find the larger number 

2a3f12232f10bdccc948b67c6bb2a711b88d8042
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-03-22 13:33:39 -0700

    Complete exercise 07_tempConversion to where it passes all 6 tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  const tempC = Number(((tempF-32)*(5/9)).toFixed(1));
+  return tempC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  const tempF = Number((tempC*(9/5)+32).toFixed(1));
+  return tempF;
 };
 
 // Do not edit below this line

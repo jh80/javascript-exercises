dc84ca2ae2b876c025250d1e929a24df2f1b2960
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-04-05 13:39:01 -0700

    Include notes after looking at solutions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -23,13 +23,11 @@ const multiply = function(nums) {
 };
 
 const power = function(num, exponent) {
-	return num**exponent;
+	return num**exponent; // In solution they use return Math.pow(a,b);
 };
 
 const factorial = function(num) {
-  if (num === 0) {
-    return 1;
-  }
+  if (num === 0) return 1; // I originally had curly braces here, but their solution put it in one line so they didn't need the curly braces, so I changed mine to practice doing that too
   let total = 1;
   for (let i = 1; i <= num; i++) {
     total *= i;

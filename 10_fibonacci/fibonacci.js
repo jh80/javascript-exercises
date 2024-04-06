af69799dd2a68f8ffe14c44c07168069156aab8a
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-04-05 17:05:57 -0700

    Complete fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
 
+const fibonacci = function(index) {
+    let fibNum = "OOPS";
+    if (index == 0) {
+        fibNum = 0;
+    } else if (index == 1) {
+        fibNum = 1;
+    } else if ( index > 1) {
+        fibNum = (fibonacci(index-1) +fibonacci(index-2));
+    }
+    return fibNum;
 };
 
 // Do not edit below this line

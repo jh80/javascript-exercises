d334b4f78b5d858688757e5425ec25d10f77d555
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-04-07 09:49:47 -0700

    Complete findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+// map to make an array of ages
+// reduce to search for the oldest
 
+// reduce array 
+
+const findTheOldest = function(array) {
+    const oldest = array.reduce(((last, curr) => {
+        const d = new Date();
+        const thisYear = d.getFullYear();
+        let currEndYear;
+        let lastEndYear;
+        if (curr.yearOfDeath) {
+            currEndYear = curr.yearOfDeath;
+        } else {
+            currEndYear = thisYear;
+        }
+        if (last.yearOfDeath) {
+            lastEndYear = last.yearOfDeath;
+        } else {
+            lastEndYear = thisYear;
+        }
+        const currAge = currEndYear - curr.yearOfBirth;
+        const lastAge = lastEndYear - last.yearOfBirth
+        return (currAge > lastAge) ? curr : last;
+    }))
+    return oldest;
 };
 
 // Do not edit below this line
